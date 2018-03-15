c55a2f32370fad155f162a6037e5f37bb457bde7
Abhishek Mishra <abmishra2111@gmail.com>
2018-03-15 23:39:09 +0530

    Implementation of OAuth2 security

--- src/main/java/com/online/test/common/ResourcesURI.java
+++ src/main/java/com/online/test/common/ResourcesURI.java
@@ -2,7 +2,7 @@ package com.online.test.common;
 
 public class ResourcesURI {
 	
-	public static final String QUESTIONS_URI = "/v1/questions";
+	public static final String QUESTIONS_URI = "/oauth2/v1/questions";
 	public static final String QUESTION_DETAILS = "/v1/rooms/{roomId}";
 
 }
--- src/main/java/com/online/test/config/SecurityConfiguration.java
+++ src/main/java/com/online/test/config/SecurityConfiguration.java
@@ -1,33 +1,49 @@
 package com.online.test.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
-
-@EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy;
+ 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-
-		http.cors().and().csrf().disable().authorizeRequests().anyRequest().permitAll().and().httpBasic();
-
-	}
-	
-
-	@Bean
-	public WebMvcConfigurerAdapter corsConfigurer() {
-		return new WebMvcConfigurerAdapter() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE").allowedOrigins("*")
-						.allowedHeaders("*");
-			}
-		};
-	}
+ 
+    private static String realName = "REALM";
+    private final static String ROLE_ADMIN = "ADMIN";
+    private final static String ROLE_USER = "USER";
+    private final String uriPattern = "/oauth2/**";
+     
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder authBuilder) throws Exception {
+    	authBuilder.inMemoryAuthentication().withUser("admin").password("Admin@123").roles(ROLE_ADMIN);
+    	authBuilder.inMemoryAuthentication().withUser("user").password("User@123").roles(ROLE_USER);
+    }
+     
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+  
+      http.csrf().disable()
+        .authorizeRequests()
+        .antMatchers(this.uriPattern).hasAnyRole(ROLE_USER, ROLE_ADMIN)
+        .and().httpBasic().realmName(realName).authenticationEntryPoint(this.getBasicAuthEntryPoint())
+        .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);//We don't need sessions to be created.
+    }
+     
+    @Bean
+    public CustomBasicAuthenticationEntryPoint getBasicAuthEntryPoint(){
+        return new CustomBasicAuthenticationEntryPoint();
+    }
+     
+    /* To allow Pre-flight [OPTIONS] request from browser */
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(HttpMethod.OPTIONS, "/**");
+    }
 }
